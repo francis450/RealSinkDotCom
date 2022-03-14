c2aaa18d45b5b3e43249b47f775a11975250f2a4
francis kamande <franciskamande2001@gmail.com>
2022-03-14 11:51:44 +0300

    arrayList to hold location of the cells where the DotCOm resides

--- DotCom.java
+++ DotCom.java
@@ -3,4 +3,6 @@ import java.util.*;
 public class DotCom{
 	//A string to hold the name of the Dot Com
 	private String name;
+	//An arrayList to hold location of the cells where the DotCOm resides
+	private ArrayList<String> locationCells;
 }
