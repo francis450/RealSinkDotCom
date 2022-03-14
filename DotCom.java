3900d922c17b1cf71315c5837e5e7a81fc4f4ed0
francis kamande <franciskamande2001@gmail.com>
2022-03-14 11:53:36 +0300

    A getter method to get the name of the DotCom

--- DotCom.java
+++ DotCom.java
@@ -3,10 +3,17 @@ import java.util.*;
 public class DotCom{
 	//A string to hold the name of the Dot Com
 	private String name;
+	
 	//An arrayList to hold location of the cells where the DotCOm resides
 	private ArrayList<String> locationCells;
+	
 	//A setter for the name Instance variable
 	public void setName(String jina){
 		name = jina;
+	}
+
+	//A getter method to get the name of the DotCom
+	public String getName(){
+		return name;
 	}
 }
