61198e9cec07062765eb5dcb1bbff68ba930e4a5
francis kamande <franciskamande2001@gmail.com>
2022-04-22 17:02:59 +0300

    Make a string to hold the alphabetical notation of the columns in the grid

--- GameHelper.java
+++ GameHelper.java
@@ -2,3 +2,8 @@
 import java.util.*;
 //import java input and output streams
 import java.io.*;
+public class GameHelper{
+	//Make a string to hold the alphabetical notation of the columns in the grid
+	private static final String alphabet = "abcdefg";
+
+}
