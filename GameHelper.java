5cea255b167e4d995adb061adf8d0dd51f7bbc2c
francis kamande <franciskamande2001@gmail.com>
2022-04-23 17:08:19 +0300

    an int to define the length of the vitual grid

--- GameHelper.java
+++ GameHelper.java
@@ -5,5 +5,7 @@ import java.io.*;
 public class GameHelper{
 	//Make a string to hold the alphabetical notation of the columns in the grid
 	private static final String alphabet = "abcdefg";
+	//Make an int to define the length of the vitual grid
+	private int gridLength = 7;
 
 }
